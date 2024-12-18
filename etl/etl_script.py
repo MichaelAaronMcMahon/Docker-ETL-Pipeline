b7d55ad1bd9fee34d24f7f1771c4e60833f4f7cc
Michael McMahon <michaelaaronmcmahon@gmail.com>
2024-12-17 23:14:01 -0500

    First commit

---
+++ etl/etl_script.py
@@ -0,0 +1,65 @@
+import subprocess
+import time
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import text
+import re
+
+def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            result = subprocess.run(
+                ["pg_isready", "-h", host], check=True, capture_output=True, text=True)
+            if "accepting connections" in result.stdout:
+                print("Successfully connected to PostgreSQL!")
+                return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error connecting to PostgreSQL: {e}")
+            retries += 1
+            print(
+                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
+            time.sleep(delay_seconds)
+    print("Max retries reached. Exiting.")
+    return False
+
+
+if not wait_for_postgres(host="source_postgres"):
+    exit(1)
+
+print("Starting ETL script...")
+
+source_config = {
+    'dbname': 'source_db',
+    'user': 'postgres',
+    'password': 'McMahon',
+    'host': 'source_postgres',
+    'port': '5433:5432'
+}
+
+destination_config = {
+    'dbname': 'destination_db',
+    'user': 'postgres',
+    'password': 'McMahon',
+    'host': 'destination_postgres',
+    'port': '5434:5432'
+}
+
+source_engine = create_engine(url="postgresql://{0}:{1}@{2}/{3}".format(
+    source_config['user'], source_config['password'], source_config['host'], source_config['dbname']
+))
+
+destination_engine = create_engine(url="postgresql://{0}:{1}@{2}/{3}".format(
+    destination_config['user'], destination_config['password'], destination_config['host'], destination_config['dbname']
+))
+
+df = pd.read_sql_query('select * from users', source_engine)
+df["first_name"] = df["first_name"].apply(lambda x: re.sub("\s.*", "", x))
+df["last_name"] = df["last_name"].apply(lambda x: re.sub("\s.*", "", x))
+
+df.to_sql("Users", destination_engine)
+with destination_engine.connect() as connection:
+    connection.execute(text("ALTER TABLE \"Users\" ADD PRIMARY KEY (id);"))
+    connection.commit()
+
+print("Ending ETL script...")
---
+++ source_db_init/init.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    date_of_birth DATE
+);
+
+INSERT INTO users (first_name, last_name, email, date_of_birth) VALUES
+('Alex', 'Smith', 'alexsmith@gmail.com', '1990-03-01'),
+('Robert', 'Waters', 'florist112@gmail.com', '1982-05-25'),
+('Roger', 'White', 'bookworm24@yahoo.com', '1986-10-21'),
+('Walter A.', 'Hunter', 'whunt404@yahoo.com', '1976-10-01'),
+('Kenneth', 'Black', 'kennethblack@gmail.com', '1999-07-02'),
+('Marie', 'Johnson', 'mariesworld@gmail.com.com', '1987-02-14'),
+('Peter B.', 'Raleigh', 'peterr24@yahoo.com', '1986-02-21'),
+('Michael', 'McMahon', 'michaelaaronmcmahon@gmail.com', '1995-06-05'),
+('Eileen', 'Terrence', 'sarahlewis@gmail.com', '1989-03-25'),
+('Jasmine', 'Jian Jr.', 'jjianjr@gmail.com', '1999-02-15'),
+('Joanne', 'Walker', 'joannejj@gmail.com', '1992-11-12'),
+('Mitchell', 'Whitman', 'mitchelwhitman@yahoo.com', '1996-08-08'),
+('Joseph', 'Carole', 'jcarole66@gmail.com', '1984-04-20'),
+('Harry', 'Baldwin', 'hbaldwin@gmail.com', '1993-12-30'),
+('Larry', 'Lincoln III', 'thirdllincoln@gmail.com', '1998-05-30'),
+('Marissa', 'Patrick', 'mpat@yahoo.com', '1990-09-15'),
+('Stephanie', 'Wright', 'stephwright@yahoo.com', '1997-05-10'),
+('Jennifer', 'Katz', 'jenniferkatz@gmail.com', '1986-07-22');
+
+-- CREATE TABLE authors (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     country_of_origin VARCHAR(50),
+--     date_of_birth DATE
+-- )
+
+-- INSERT INTO authors (first_name, last_namem country_of_origin, date_of_birth) VALUES
+-- ("Fyodor", "Dostoevsky", "Russia", "11-11-1821"),
+-- ("George", "Orwell", "England", )
